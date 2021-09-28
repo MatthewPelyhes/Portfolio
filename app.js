94254bc68a1f4632e9ce88df0f11c0dcb7a4ad98
Matthew Pelyhes <73857687+MatthewPelyhes@users.noreply.github.com>
2021-09-28 09:16:29 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const showNew = document.querySelector('#show-new');
+
+const content = `
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content incoming
+Lots of content ${filler}
+`
+
+showNew.addEventListener('click', ()=>{
+  console.log("clicked showNew")
+  Intercom('showNewMessage', content)
+})
